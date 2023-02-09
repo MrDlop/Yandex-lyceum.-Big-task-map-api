5820690257650ff5f85c55f0ea95215b83f6c0a5
MrDlop <1112.mimil@gmail.com>
2023-02-09 15:01:40 +0500

    Refactor code and add functional

--- getter_map.py
+++ getter_map.py
@@ -1,13 +1,14 @@
 from io import BytesIO
 import requests
+from typing import Tuple
 
 
-def map_for_coords(coords: list[float, float],
+def map_for_coords(coords: Tuple[float, float],
                    scale: float = 2,
                    type_map: str = "map",
                    **kwargs) -> BytesIO:
     """
-    :param coords: object coordinates [x, y]
+    :param coords: object coordinates (x, y)
     :param scale: scale image (1-4)
     :param type_map: map/sat/skl/trf
     :param kwargs: other param for static-maps
@@ -15,7 +16,7 @@ def map_for_coords(coords: list[float, float],
     """
 
     # search map URL
-    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    map_api_server = "https://static-maps.yandex.ru/1.x/"
 
     # params for search in static-maps
     map_params = {
@@ -33,20 +34,14 @@ def map_for_coords(coords: list[float, float],
         response.content)
 
 
-def search_name(obj: str,
-                scale: float = 2,
-                type_map: str = "map",
-                point: bool = True) -> BytesIO:
+def search_name(obj: str) -> Tuple[float, float]:
     """
-    :param point: point for object
-    :param type_map: map/sat/skl/trf
-    :param scale: scale image (1-4)
     :param obj: name object (for example: "Уфа")
-    :return: BytesIO map
+    :return: coords object
     """
 
     # search map URL
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_api_server = "https://geocode-maps.yandex.ru/1.x/"
 
     # params for search in geocode-maps
     geocoder_params = {
@@ -62,27 +57,8 @@ def search_name(obj: str,
     # cut json response
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
-    # coords center
-    toponym_coordinates = toponym["Point"]["pos"]
-    # latitude and longitude
-    toponym_longitude, toponym_latitude = toponym_coordinates.split(" ")
 
-    tp = [list(map(float, toponym['boundedBy']['Envelope'][i].split())) for i in toponym['boundedBy']['Envelope']]
-    dx = str(abs(tp[0][0] - tp[1][0]))
-    dy = str(abs(tp[0][1] - tp[1][1]))
-
-    # # params for search in static-maps
-    map_params = {
-        "spn": ",".join([dx, dy]),
-    }
-
-    if point:
-        map_params["pt"] = f"{toponym['Point']['pos'].replace(' ', ',')},round"
-
-    return map_for_coords([toponym_longitude, toponym_latitude],
-                          scale=scale,
-                          type_map=type_map,
-                          **map_params)
+    return toponym['Point']['pos']
 
 
 def toponym_obj(obj: str) -> dict:
@@ -90,7 +66,7 @@ def toponym_obj(obj: str) -> dict:
     :param obj: object
     :return: toponym object
     """
-    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_api_server = "https://geocode-maps.yandex.ru/1.x/"
 
     geocoder_params = {
         "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
--- main.py
+++ main.py
@@ -2,32 +2,49 @@ import sys
 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
-from PIL.ImageQt import ImageQt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
-from interface.interface_main import Ui_MainWindow
+from PIL.ImageQt import ImageQt
 from PIL import Image
 
 from getter_map import *
+from interface.interface_main import Ui_MainWindow
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
+    scale: float
+    coord_x: float
+    coord_y: float
+    type_map: str
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.coord_x, self.coord_y = 0, 0
+
+        self.coord_x, self.coord_y = 55.958736, 54.735152
+        self.point = None
+        self.type_map = "map"
         self.scale = 1
 
-        self.pushButton_searh.clicked.connect(self.pushButton_searh_clicked)
-
-    def pushButton_searh_clicked(self):
-        self.refactor_coords()
-        image = map_for_coords([self.coord_x, self.coord_y], scale=self.scale)
-        image = Image.open(image)
-        self.label_map.setPixmap(QPixmap.fromImage(ImageQt(image)))
+        self.pushButton_searh.clicked.connect(self.pushButton_search_clicked)
 
     def refactor_coords(self):
         self.coord_x, self.coord_y = map(float, self.lineEdit_search.text().split(","))
+
+    def update(self):
+        options = dict()
+        if not (self.point is None):
+            options["pt"] = f"{','.join(str(i) for i in self.point)},round"
+        image = map_for_coords((self.coord_x, self.coord_y),
+                               type_map=self.type_map,
+                               scale=self.scale,
+                               **options)
+        image = Image.open(image)
+        self.label_map.setPixmap(QPixmap.fromImage(ImageQt(image)))
+
+    def pushButton_search_clicked(self):
+        self.refactor_coords()
+        self.update()
 
     def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
         if a0.key() == Qt.Key_PageUp:
@@ -41,17 +58,39 @@ class MyWidget(QMainWindow, Ui_MainWindow):
                 self.scale = 1
 
         if a0.key() == Qt.Key_Up:
-            self.coord_y = (self.coord_y + 5 + 90) % 180 - 90
+            self.coord_y = self.coord_y + 0.0001
+            if self.coord_y > 90:
+                self.coord_y = 90
         if a0.key() == Qt.Key_Down:
-            self.coord_y = (self.coord_y - 5 + 90) % 180 - 90
-        if a0.key() == Qt.Key_Left:
-            self.coord_x = (self.coord_x + 5) % 180
-        if a0.key() == Qt.Key_Right:
-            self.coord_x = (self.coord_x - 5) % 180
+            self.coord_y = self.coord_y - 0.0001
+            if self.coord_y < -90:
+                self.coord_y = -90
+        if a0.key() == Qt.Key_Right or a0.key() == Qt.Key_6:
+            self.coord_x = self.coord_x + 0.0001
+            if self.coord_x > 90:
+                self.coord_x = -90
+        if a0.key() == Qt.Key_Left or a0.key() == Qt.Key_4:
+            self.coord_x = self.coord_x - 0.0001
+            if self.coord_x < -90:
+                self.coord_x = 90
 
-        image = map_for_coords([self.coord_x, self.coord_y], scale=self.scale)
-        image = Image.open(image)
-        self.label_map.setPixmap(QPixmap.fromImage(ImageQt(image)))
+        self.update()
+
+    # No active prototype
+    # -------------------------------------------------------------------------------
+    def replace_type(self):
+        self.type_map = self.sender().text()
+        self.update()
+
+    def add_search(self):
+        self.point = search_name(self.sender().text())
+        self.update()
+
+    def delete_search(self):
+        self.point = None
+        self.update()
+
+    # -------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
