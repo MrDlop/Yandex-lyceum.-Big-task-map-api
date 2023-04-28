7926869b90f587eecec883b51a952454acafc25c
MrDlop <72319098+MrDlop@users.noreply.github.com>
2023-04-28 20:59:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,128 +1,83 @@
-import os
-import sys
-
-from PyQt5 import QtGui, uic
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap
-from PIL.ImageQt import ImageQt
-from PIL import Image
-
-from getter_map import *
-# from interface.interface_main import Ui_MainWindow
-from settings_main import SettingsForm
+from io import BytesIO
+import requests
+from typing import Tuple, List
 
 
-class MyWidget(QMainWindow):
-    scale: float
-    coord_x: float
-    coord_y: float
-    type_map: str
+def map_for_coords(coords: Tuple[float, float],
+                   scale: float = 2,
+                   type_map: str = "sat",
+                   **kwargs):
+    """
+    :param coords: object coordinates (x, y)
+    :param scale: scale image (1-4)
+    :param type_map: map/sat/skl/trf
+    :param kwargs: other param for static-maps
+    :return: BytesIO map
+    """
 
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('interface/interface_main.ui', self)
+    # search map URL
+    map_api_server = "https://static-maps.yandex.ru/1.x/"
 
-        self.coord_x, self.coord_y = 37.677751, 55.757718
-        self.point = None
-        self.type_map = "map"
-        self.scale = 1
-        self.idx = ""
-        self.address = ""
-        self.check = False
+    # params for search in static-maps
+    map_params = {
+        "ll": ','.join(str(i) for i in coords),
+        "l": type_map,
+        "scale": str(scale)
+    }
+    for i in kwargs:
+        map_params[i] = kwargs[i]
 
-        self.pushButton_searh.clicked.connect(self.pushButton_search_clicked)
-        self.pushButton_settings.clicked.connect(self.pushButton_settings_clicked)
-        self.pushButton_cancel.clicked.connect(self.pushButton_cancel_clicked)
-        self.checkBox_index.stateChanged.connect(self.index_clicked)
-        self.update()
+    # service call
+    response = requests.get(map_api_server, params=map_params)
 
-    def index_clicked(self, state):
-        self.check = not self.check
-        if state == Qt.Checked:
-            if not (self.point is None):
-                self.label.setText(self.address + " " + self.idx)
-        else:
-            self.label.setText(self.address)
-
-    def refactor_coords(self):
-        self.point = search_coords_for_name(self.lineEdit_search.text())
-        self.address = address_obj(self.lineEdit_search.text())
-        self.idx = postal_number_obj(self.lineEdit_search.text())
-        if self.check:
-            self.label.setText(self.address + " " + self.idx)
-        else:
-            self.label.setText(self.address)
-        self.coord_x, self.coord_y = self.point
-        self.update()
-
-    def update(self):
-        options = dict()
-        if not (self.point is None):
-            options["pt"] = f"{','.join(str(i) for i in self.point)},round"
-
-        image = BytesIO(map_for_coords((self.coord_x, self.coord_y),
-                                       type_map=self.type_map,
-                                       scale=self.scale,
-                                       **options).content)
-        image = Image.open(image)
-        self.label_map.setPixmap(QPixmap.fromImage(ImageQt(image)))
-
-    def pushButton_search_clicked(self):
-        self.refactor_coords()
-        self.update()
-
-    def pushButton_cancel_clicked(self):
-        self.point = None
-        self.address = ""
-        self.idx = ""
-        self.label.setText("")
-        self.update()
-
-    def pushButton_settings_clicked(self):
-        self.form = SettingsForm(self)
-        self.form.show()
-
-    def keyPressEvent(self, a0: QtGui.QKeyEvent) -> None:
-        if a0.key() == Qt.Key_PageUp:
-            self.scale += 0.1
-            if self.scale > 4:
-                self.scale = 4
-
-        if a0.key() == Qt.Key_PageDown:
-            self.scale -= 0.1
-            if self.scale < 1:
-                self.scale = 1
-            self.update()
-
-        if a0.key() == Qt.Key_Up or a0.key() == Qt.Key_8:
-            self.coord_y = self.coord_y + 0.0001
-            if self.coord_y > 90:
-                self.coord_y = 90
-            self.update()
-
-        if a0.key() == Qt.Key_Down or a0.key() == Qt.Key_2:
-            self.coord_y = self.coord_y - 0.0001
-            if self.coord_y < -90:
-                self.coord_y = -90
-            self.update()
-
-        if a0.key() == Qt.Key_Right or a0.key() == Qt.Key_6:
-            self.coord_x = self.coord_x + 0.0001
-            if self.coord_x > 90:
-                self.coord_x = -90
-            self.update()
-
-        if a0.key() == Qt.Key_Left or a0.key() == Qt.Key_4:
-            self.coord_x = self.coord_x - 0.0001
-            if self.coord_x < -90:
-                self.coord_x = 90
-            self.update()
+    return response
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    app.addLibraryPath(os.getcwd() + "imageformats")
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+def toponym_obj(obj: str) -> dict:
+    """
+    :param obj: object
+    :return: toponym object
+    """
+    geocoder_api_server = "https://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": obj,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    json_response = response.json()
+
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    return toponym
+
+
+def search_coords_for_name(obj: str) -> List[float]:
+    """
+    :param obj: name object (for example: "Уфа")
+    :return: coords object
+    """
+    toponym = toponym_obj(obj)
+    return list(map(float, toponym['Point']['pos'].split(' ')))
+
+
+def address_obj(obj: str) -> str:
+    """
+    :param obj: object
+    :return: full formatted address
+    """
+    toponym = toponym_obj(obj)
+    return toponym['metaDataProperty']['GeocoderMetaData']['Address']['formatted']
+
+
+def postal_number_obj(obj: str) -> str:
+    """
+    :param obj: object
+    :return: postal number address
+    """
+    toponym = toponym_obj(obj)
+    if 'postal_code' not in toponym['metaDataProperty']['GeocoderMetaData']['Address']:
+        return ""
+    return toponym['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
